643796f3c98bc621240aa08e5adc4cc309b2c2d6
Benjamin <carlosbpac@gmail.com>
2024-08-25 14:01:55 -0600

    chore: Update network configuration script for load balancing

---
+++ balanceador/conf_red_balanceador.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo iptables -t nat -F POSTROUTING
+#ISP 1
+sudo iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
+#ISP 2
+sudo iptables -t nat -A POSTROUTING -o enp8s0 -j MASQUERADE
+
+#Reinicio del servicio de networking
+sudo systemctl restart networking
+
+# Limpieza de tablas de reenvio
+sudo iptables -F FORWARD
+
+# Reglas para permitir el trafico de reenvio de datos en las interfaces
+sudo iptables -A FORWARD -i enp7s0 -o enp1s0 -j ACCEPT
+sudo iptables -A FORWARD -i enp1s0 -o enp7s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i enp7s0 -o enp8s0 -j ACCEPT
+sudo iptables -A FORWARD -i enp8s0 -o enp7s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Verificacion de las tablas y enrutamiento de red
+ip route
+
+sudo iptables -t nat -L -v -n
+
+sudo iptables -L FORWARD -v -n
