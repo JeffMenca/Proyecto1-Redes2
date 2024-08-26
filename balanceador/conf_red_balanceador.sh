9ddc04b506d3c454ded9d5b3e71a33dfe94c867c
Benjamin <carlosbpac@gmail.com>
2024-08-25 18:13:16 -0600

    Update network configuration script for load balancing

--- balanceador/conf_red_balanceador.sh
+++ balanceador/conf_red_balanceador.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+# Script para reemplazar el contenido de /etc/nftables.conf
+
+sudo bash -c 'cat << EOF > /etc/nftables.conf
+#!/usr/sbin/nft -f
+flush ruleset
+
+table inet filter {
+    chain input {
+        type filter hook input priority filter; policy accept;
+    }
+    chain forward {
+        type filter hook forward priority filter; policy accept;
+    }
+    chain output {
+        type filter hook output priority filter; policy accept;
+    }
+}
+
+# Manejo de Carga (Tráfico en Interfaces enp7s0 y enp8s0 que salen a los ISPs)
+table ip loadbalance {
+    chain prerouting {
+        type filter hook prerouting priority 0; policy accept;
+
+        # Marcar el tráfico para balanceo de carga
+        ip saddr 192.168.45.0/24 ip daddr != 192.168.45.0/24 mark set 1 counter
+    }
+
+    chain output {
+        type route hook output priority 0; policy accept;
+
+        # Balanceo de carga usando marcas
+        meta mark set numgen random mod 2
+        meta mark 0 oif "enp7s0"
+        meta mark 1 oif "enp8s0"
+    }
+}
+EOF'
+
+echo "Contenido de /etc/nftables.conf actualizado con éxito."
+
+sudo nft -f /etc/nftables.conf
+
 sudo iptables -t nat -F POSTROUTING
 #ISP 1
 sudo iptables -t nat -A POSTROUTING -o enp7s0 -j MASQUERADE
