aea96d59c5240edd01b09d7838fbb81c622c77d8
Benjamin <carlosbpac@gmail.com>
2024-08-29 19:39:59 -0600

    Update network configuration script for load balancing

--- balanceador/conf_red_balanceador2.sh
+++ balanceador/conf_red_balanceador2.sh
@@ -64,8 +64,8 @@ sudo iptables -A FORWARD -i enp8s0 -o enp9s0 -m state --state RELATED,ESTABLISHE
 sudo ip route del default via 192.168.25.1 dev enp8s0
 
 # Agregar default via para ambas interfaces (enp7s0 y enp8s0) hacia ISP1 y ISP2
-# sudo ip route add default nexthop via 192.168.20.1 dev enp7s0 weight 2 nexthop via 192.168.25.1 dev enp8s0 weight 2
-sudo ip route add default scope global nexthop via 192.168.20.1 dev enp7s0 weight 1 nexthop via 192.168.25.1 dev enp8s0 weight 1
+sudo ip route add default nexthop via 192.168.20.1 dev enp7s0 weight 2 nexthop via 192.168.25.1 dev enp8s0 weight 2
+# sudo ip route add default scope global nexthop via 192.168.20.1 dev enp7s0 weight 1 nexthop via 192.168.25.1 dev enp8s0 weight 1
 
 # Verificacion de las tablas y enrutamiento de red
 ip route
